a4a4de562b1509de900272bd25da99ab71924d04
hubertme <hubert.tatra@gmail.com>
2018-09-07 00:05:13 +0700

    Added "Template.java"
    Template used for everyday use :)

---
+++ Template.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+// Change "Template" to your file name
+class Template{
+	public static void main(String[] args) throws java.lang.Exception {
+		// Declare scanner
+		Scanner input = new Scanner(System.in);
+
+		// Declare variables here
+
+
+		// Some code here
+		
+	}
+}
