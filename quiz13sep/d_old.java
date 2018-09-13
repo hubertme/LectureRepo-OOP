ee64e9f36f050da7a7b9430d1c3ff037aa34a45c
Hubert Wang <hubert.tatra@gmail.com>
2018-09-13 18:49:03 +0700

    Added old d.java
    For comparing purpose

---
+++ quiz13sep/d_old.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class d_old{
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.format("Input a number: ");
+		int limit = input.nextInt();
+
+		for (int i=2; i<=limit; i++){
+			int count = 0;
+
+			// System.out.println(sqrt);
+
+			for (int j=2;j<i;j++){
+				if (i%j==0){
+					count+=1;
+				}
+			}
+
+			if (count==0){
+				System.out.println(i);
+			}
+		}
+	}
+}
